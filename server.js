d4549d1c296e8912a65cf8a5ed37508b8c26dae5
KartikRajaS <kartiksubu.krs@gmail.com>
2016-06-04 17:35:43 +0530

    date and time

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ console.log("Example app listening at http://%s:%s", host, port)
 
 app.post('/query-post1',  urlencodedParser,function (req, res)
 {
-  var dv={"school_id":req.query.school_id,"query_id":req.query.query_id,"student_id":req.query.student_id,"parent_name":req.query.name,"parent_email":req.query.email,"category":req.query.category,"query_message":req.query.complaint,"query_status":req.query.status};
+  var dv={"school_id":req.query.school_id,"query_id":req.query.query_id,"query_reply":"","student_id":req.query.student_id,"parent_name":req.query.name,"parent_email":req.query.email,"category":req.query.category,"query_message":req.query.complaint,"query_status":req.query.status,"updated_date":req.query.date,"time":req.query.time};
 
   //console.log(school_id);
        connection.query('insert into query set ?',[dv],
@@ -58,7 +58,7 @@ app.post('/query-post1',  urlencodedParser,function (req, res)
     }
     else
     {
-
+      console.log(err);
       res.status(200).json({'returnval': 'invalid'});
     }
   
