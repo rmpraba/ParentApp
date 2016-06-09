68cecf0906e6c73e279010b2baa79ae723fbae12
tamils30 <ntamilselvimca@gmail.com>
2016-06-09 17:35:23 +0530

    drawer panel for helpdesk

--- server.js
+++ server.js
@@ -221,7 +221,7 @@ app.post('/parentinbox',  urlencodedParser,function (req, res)
   var school_id={"school_id":req.query.schol};
   var stud={"student_id":req.query.id};
   var status={"query_status":req.query.status};
-  //console.log(school_id+   +stud);
+  console.log(school_id+''+stud+''+status);
        connection.query('select * from query where ? and ? and ? and query_reply!=""',[school_id,stud,status],
         function(err, rows)
         {
